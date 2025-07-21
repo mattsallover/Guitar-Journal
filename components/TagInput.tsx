7a6c53e374df0eea16e41b2b336c52d2fa40a7bc
mattsallover <levine1520@gmail.com>
2025-07-21 06:48:45 -0500

    Updated TagInput.tsx

--- components/TagInput.tsx
+++ components/TagInput.tsx
@@ -116,27 +116,3 @@ export const TagInput: React.FC<TagInputProps> = ({ values, onChange, suggestion
     </div>
   );
 };
-          value={inputValue}
-          onChange={handleInputChange}
-          onKeyDown={handleKeyDown}
-          onFocus={() => inputValue && setShowSuggestions(true)}
-          placeholder={values.length > 0 ? '' : placeholder}
-          className="flex-grow bg-transparent p-1 focus:outline-none min-w-[120px] h-8"
-        />
-      </div>
-      {showSuggestions && filteredSuggestions.length > 0 && (
-        <ul className="absolute z-10 w-full mt-1 bg-surface border border-border rounded-md shadow-lg max-h-60 overflow-auto">
-          {filteredSuggestions.map(suggestion => (
-            <li
-              key={suggestion}
-              onClick={() => addValue(suggestion)}
-              className="px-4 py-2 cursor-pointer hover:bg-primary/20"
-            >
-              {suggestion}
-            </li>
-          ))}
-        </ul>
-      )}
-    </div>
-  );
-};
